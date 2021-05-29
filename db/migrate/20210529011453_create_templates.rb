99751d28d386ca2068e54c05975645ac5d6377b3
waterquarks <waterquarks@gmail.com>
2021-05-28 20:15:41 -0500

    add templates model

---
+++ db/migrate/20210529011453_create_templates.rb
@@ -0,0 +1,10 @@
+class CreateTemplates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :templates do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
