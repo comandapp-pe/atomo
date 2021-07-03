0cbc7fcd60b5f681d3cde948d88e096e229eb7a2
waterquarks <waterquarks@gmail.com>
2021-07-02 20:46:12 -0500

    refactor admin_products_controller to use plain products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  before_action :set_admin_product, only: %i[ show edit update destroy ]
+  before_action :set_product, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
 
   # GET /admin/products or /admin/products.json
@@ -22,15 +22,15 @@ class Admin::ProductsController < ApplicationController
 
   # POST /admin/products or /admin/products.json
   def create
-    @admin_product = Product.new(admin_product_params)
+    @product = Product.new(admin_product_params)
 
     respond_to do |format|
-      if @admin_product.save
-        format.html { redirect_to @admin_product, notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @admin_product }
+      if @product
+        format.html { redirect_to @product, notice: "Product was successfully created." }
+        format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,19 +38,19 @@ class Admin::ProductsController < ApplicationController
   # PATCH/PUT /admin/products/1 or /admin/products/1.json
   def update
     respond_to do |format|
-      if @admin_product.update(admin_product_params)
-        format.html { redirect_to @admin_product, notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @admin_product }
+      if @product.update(admin_product_params)
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
+        format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /admin/products/1 or /admin/products/1.json
   def destroy
-    @admin_product.destroy
+    @product.destroy
     respond_to do |format|
       format.html { redirect_to admin_products_url, notice: "Product was successfully destroyed." }
       format.json { head :no_content }
@@ -60,8 +60,8 @@ class Admin::ProductsController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_admin_product
-    @admin_product = Product.find(params[:id])
+  def set_product
+    @product = Product.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
