2c0b2f89c5a575530aee774d8aa22e366287a728
waterquarks <waterquarks@gmail.com>
2021-07-10 12:54:13 -0500

    patch typo

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,7 +9,7 @@ class Admin::SessionsController < ApplicationController
 
     respond_to do |format|
       if @admin_session
-        format.html { redirect_to admin_orders_url, notice: 'Ya tenías una sesión activa, así que fuiste redireccionado.' }
+        format.html { redirect_to admin_orders_url, notice: 'Ya tenías una sesión activa, así que fuiste redireccionado automáticamente.' }
       else
         @admin_session = Admin::Session.new
 
