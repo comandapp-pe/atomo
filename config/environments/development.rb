9bb3a48de325cb3932323923091a990e47a71ea1
waterquarks <waterquarks@gmail.com>
2021-10-05 17:02:57 -0500

    change log settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.i18n.default_locale = :es
   config.i18n.fallbacks = [:en]
 
-  config.after_initialize do
-    ActiveRecord::Base.logger.level = 1 # Doesn't log database queries
-  end
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
+  config.log_level = :info
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,4 +130,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.assets.compile = false
+
+  # Do not log whether partials rendered
+  config.action_view.logger = nil
 end
