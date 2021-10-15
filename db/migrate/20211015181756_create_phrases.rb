7e69b461d05a42a2fdd2a9a5d5771b038de85bab
waterquarks <waterquarks@gmail.com>
2021-10-15 13:19:05 -0500

    fill in the phrases migration

--- db/migrate/20211015181756_create_phrases.rb
+++ db/migrate/20211015181756_create_phrases.rb
@@ -1,6 +1,8 @@
 class CreatePhrases < ActiveRecord::Migration[6.1]
   def change
     create_table :phrases do |t|
+      t.references :order, null: false, foreign_key: true
+      t.text :content
 
       t.timestamps
     end
