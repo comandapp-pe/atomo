c765dd32ce5318e3f97c8a0d18e3fde6078c0bf7
waterquarks <waterquarks@gmail.com>
2021-10-21 12:25:13 -0500

    show logo in email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,6 +5,8 @@ class OrderMailer < ApplicationMailer
   def confirmation_email
     @order = params[:order]
 
+    attachments.inline['logo.png'] = File.read('/Users/ioaquine/Downloads/quipu/socialspots/app/assets/images/logo.png')
+
     mail to: @order.customer_email, subject: "Tu orden en Social Spots fue confirmada"
   end
 end
