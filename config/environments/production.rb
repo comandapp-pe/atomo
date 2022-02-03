6e0c41efd7d464a90f2045e272672b269a0af6d8
victorhs98 <vmhs1998@gmail.com>
2022-02-03 09:15:22 -0500

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,15 +131,6 @@ Rails.application.configure do
 
   config.assets.compile = false
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)  
-config.serve_static_assets = false
-
-# Compress JavaScripts and CSS  
-config.assets.compress = true
-
-  # Generate digests for assets URLs  
-config.assets.digest = true
-
   # Do not log whether partials rendered
   config.action_view.logger = nil
 
